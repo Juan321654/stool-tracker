ca432dfa4aac60bd2b936f844b50036cf6f04e7c
Juan Camacho <JuanCamachopers@hotmail.com>
2020-09-06 19:39:51 -0400

    moved app.js to showreview

--- stool-tracker-app/src/App.js
+++ stool-tracker-app/src/App.js
@@ -1,37 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Review from './Review';
+import React from 'react';
+import ShowReview from './ShowReview';
 import CreateReview from './CreateReview';
 import './App.css';
 
 
 function App() {
-  const [reviews, setReviews] = useState([]);
 
-  useEffect(() => {
-    const getReviews = async () => {
-      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/stool-tracker`;
-      const response = await axios.get(airtableURL, {
-        headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-        },
-      });
-      setReviews(response.data.records);
-      console.log(response.data.records);
-
-    }
-    getReviews();
-  }, []);
 
   return (
     <div>
-      <div className="App">
-        {
-          reviews.map((review) => (
-            <Review review={review} key={review.id} />
-          ))
-        }
-      </div>
+      <ShowReview />
       <CreateReview />
     </div>
 
---
+++ stool-tracker-app/src/ShowReview.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Review from './Review';
+import axios from 'axios';
+
+function ShowReview() {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        const getReviews = async () => {
+            const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/stool-tracker`;
+            const response = await axios.get(airtableURL, {
+                headers: {
+                    'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+                },
+            });
+            setReviews(response.data.records);
+            console.log(response.data.records);
+
+        }
+        getReviews();
+    }, []);
+
+    return (
+        <div>
+            <div className="App">
+                {
+                    reviews.map((review) => (
+                        <Review review={review} key={review.id} />
+                    ))
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default ShowReview
