7e0a3f7f11b0b0b46b584145231756606eb62cc2
Juan Camacho <kmilokmacho@hotmail.com>
2020-09-08 17:25:01 -0400

    user can choose a type when writing reviews from a drop down now

--- stool-tracker-app/src/Components/CreateReview.jsx
+++ stool-tracker-app/src/Components/CreateReview.jsx
@@ -1,54 +1,68 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function CreateReview(props) {
-    const [type, setType] = useState('');
-    const [text, setText] = useState('');
+  const [type, setType] = useState("");
+  const [text, setText] = useState("");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const fields = {
-            type,
-            text
-        };
-        const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/stool-tracker`;
-        console.log(airtableURL)
-        // await axios.post(url, data, options (headers))
-        await axios.post(airtableURL, { fields }, {
-            headers: {
-                'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-                'Content-Type': 'application/json',
-            }
-        });
-        //props breaks the submit review
-        // props.setFetchReview(!props.fetchReviews);
-        setType('');
-        setText('');
-    }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const fields = {
+      type,
+      text,
+    };
+    const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/stool-tracker`;
+    console.log(airtableURL);
+    // await axios.post(url, data, options (headers))
+    await axios.post(
+      airtableURL,
+      { fields },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    //props breaks the submit review
+    // props.setFetchReview(!props.fetchReviews);
+    setType("");
+    setText("");
+  };
 
-    return (
-        <form className="user-form" onSubmit={handleSubmit}>
-            <label htmlFor="type">Type</label>
-            <input
-                name="type "
-                type="text"
-                value={type}
-                onChange={(event) => setType(event.target.value)}>
-            </input>
+  return (
+    <form className="user-form" onSubmit={handleSubmit}>
+      <label htmlFor="type">Choose a type</label>
 
-            <label htmlFor="text">Description:</label>
-            <textarea className="user-text"
-                name="text"
-                type="text"
-                value={text}
-                onChange={(event) => setText(event.target.value)}>
-            </textarea>
+      <select
+        id="type"
+        name="type"
+        value={type}
+        onChange={(event) => {
+          setType(event.target.value);
+        }}
+      >
+        <option value="Type 1">Type 1</option>
+        <option value="Type 2">Type 2</option>
+        <option value="Type 3">Type 3</option>
+        <option value="Type 4">Type 4</option>
+        <option value="Type 5">Type 5</option>
+        <option value="Type 6">Type 6</option>
+        <option value="Type 7">Type 7</option>
+      </select>
 
-            <button type="submit">Submit</button>
+      <label htmlFor="text">Description:</label>
+      <textarea
+        className="user-text"
+        name="text"
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      ></textarea>
 
-        </form>
-
-    )
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default CreateReview;
