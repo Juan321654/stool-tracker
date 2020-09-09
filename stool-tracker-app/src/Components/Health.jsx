007aa6d41d18c34462dd5db96f74923897e715ff
Juan Camacho <kmilokmacho@hotmail.com>
2020-09-09 13:09:29 -0400

    new health page displaying well

--- stool-tracker-app/src/Components/Health.jsx
+++ stool-tracker-app/src/Components/Health.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function Health() {
   return (
     <div>
-      <h1>HI</h1>
+      <h4>
+        A person's bowel habits are influenced by many factors and can change
+        day to day. A few common reasons you may experience a change in bowel
+        habits include:
+      </h4>
+      <ul>
+        <li>Diet</li>
+        <li>Stress</li>
+        <li>Dehydration</li>
+        <li>Medications</li>
+      </ul>
     </div>
   );
 }
